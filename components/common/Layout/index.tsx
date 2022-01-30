5a03a29aaeedcc10d66c2469295c240c6864dd05
unknown <saggatpwn@gmail.com>
2022-01-30 17:08:55 -0300

    Fixing component

--- components/common/Layout/index.tsx
+++ components/common/Layout/index.tsx
@@ -3,8 +3,14 @@ import {HeroContainer, HeroContent, HeroH1,
     HeroP,  
     HeroBg, VideoBg, ArrowForward, ArrowRight} from '../HeroSection/HeroElements'
 import LandingNav from '../NavBar/LandingNav';
+import { NextPage } from "next";
 
-function Layout( { children }) {
+
+type Props = {
+  className?: string,
+}
+
+const Layout : NextPage<Props> = ({ children  }) => {
   return (
     
     <main>
